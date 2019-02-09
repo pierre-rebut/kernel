7473a159a24be3092f87eb5b15b1eeb2cc460194
Insulaire des iles <pierre.rebut@epitech.eu>
2019-02-10 08:43:05 +0900

    debut ajout 42sh fonctionnel mais need fix + refonte execve (utilisation structure pour ajout d'info (dup)) + changement pid sur int

--- tools/update-img.sh
+++ tools/update-img.sh
@@ -6,4 +6,4 @@ sudo cp $2/user/ls/ls $2/user/42sh/42sh /mnt/bin
 sudo cp $2/user/utils/mount $2/user/utils/umount $2/user/utils/cat /mnt/bin
 sudo cp $2/user/roms/skate/skate.rom $2/user/roms/perrodlauncher/perrodlauncher.rom /mnt/roms
 sudo cp $2/user/roms/chichehunter/hunter.rom $2/user/roms/chichepong/pong.rom /mnt/roms
-sudo umount /mnt
+sudo umount /mnt&
