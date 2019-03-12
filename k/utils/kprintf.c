3baf26fb797d3119035f1ad458981b1a4125205b
Pierre Rebut <pierre.rebut@epitech.eu>
2019-03-12 19:50:59 +0100

    Update kprintf.c

--- k/utils/kprintf.c
+++ k/utils/kprintf.c
@@ -1,19 +1,6 @@
-/* -*- linux-c -*- ------------------------------------------------------- *
- *
- *   Copyright (C) 1991, 1992 Linus Torvalds
- *   Copyright 2007 rPath, Inc. - All Rights Reserved
- *
- *   This file is part of the Linux kernel, and is made available under
- *   the terms of the GNU General Public License version 2.
- *
- * ----------------------------------------------------------------------- */
-
-/*
- * Oh, it's a waste of space, but oh-so-yummy for debugging.  This
- * version of printf() does not include 64-bit support.  "Live with
- * it."
- *
- */
+//
+// Created by rebut_p on 20/09/18.
+//
 
 #include <stddef.h>
 #include <string.h>
